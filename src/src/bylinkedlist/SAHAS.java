ccfdfc3e1bc8611dc652c7d8c2c3f38356448f7d
bansalsahas53 <sahas.bansal_cs18@gla.ac.in>
2020-02-24 20:38:44 +0530

    first

---
+++ src/src/bylinkedlist/SAHAS.java
@@ -0,0 +1,18 @@
+package bylinkedlist;
+public interface SAHAS<E> {
+
+    void add(Person data);
+
+
+
+    void remove(Person data);
+
+
+
+    Person search(String firstName);
+
+
+
+    void print();
+
+}
